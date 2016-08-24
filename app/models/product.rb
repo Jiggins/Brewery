c71debd916b1861c68e50b3ffbb52fcb1adfbb50
Jack Higgins <jackhiggins07@gmail.com>
2016-08-24 18:57:58 +0100

    Bootstrapped the product create/edit forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
   friendly_id :name, use: :slugged
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def self.types
+    [['Hot Drink Sit In', 'Product::HotDrinkSitIn'],
+    ['Hot Drink Take Out', "Product::HotDrinkTakeOut"],
+    ['Cold Drink', "Product::ColdDrink"],
+    ['Food', "Product::Food"],
+    ['Retail', "Product::Retail"]]
+  end
 end
