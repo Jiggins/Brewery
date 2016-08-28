147d5ec771d59d1fcf9febaf7020d432ac9aad7e
Jack Higgins <jackhiggins07@gmail.com>
2016-08-28 14:19:39 +0100

    Inline forms in /products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # http://getbootstrap.com/components/#glyphicons
+  def glyphicon(glyph, options = {})
+    options[:class] = "glyphicon glyphicon-#{glyph}" + options[:class].to_s
+    options["aria-hidden"] = true
+    content_tag :span, '', options
+  end
 end
