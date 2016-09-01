320e62574bbdef5f0910abec1b6e9382ca080aa3
Jack Higgins <jackhiggins07@gmail.com>
2016-09-01 15:02:21 +0100

    Using Bootstrap alerts for create/update/delete notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,28 @@ module ApplicationHelper
     options["aria-hidden"] = true
     content_tag :span, '', options
   end
+
+  def alert(alert_level, alert_text = nil, &block)
+    unless alert_level.to_sym.in? [:success, :info, :warning, :danger]
+      raise ArgumentError.new 'Expected one of: [:success, :info, :warning or :danger]'
+    end
+
+    content_tag :div, class: ['alert', "alert-#{alert_level}", 'alert-dismissible', 'fade', 'in'], role: 'alert' do
+      button = content_tag :button, type: 'button', class: 'close', 'data-dismiss': 'alert', 'aria-label': 'Close' do
+        content_tag :span, sanitize('&times;'), 'aria-hidden': 'true'
+      end
+
+      concat content_tag :span, '', class: 'glyphicon glyphicon-exclamation-sign', 'aria-hidden': true
+      concat ' '
+      concat button
+
+      if block
+        yield block
+      elsif alert_text
+        concat alert_text
+      else
+        raise ArgumentError.new 'No text or block given'
+      end
+    end
+  end
 end
