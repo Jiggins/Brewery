3839a5a81ee1a0b655e5f249dec5603a56db0c02
Jack Higgins <jackhiggins07@gmail.com>
2016-09-12 14:13:49 +0100

    rails geerate migration addStockToProduct

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829143133) do
+ActiveRecord::Schema.define(version: 20160909173809) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160829143133) do
     t.datetime "image_updated_at"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.integer  "stock"
     t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
