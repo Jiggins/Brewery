b79dc06c777c4d1294dbe4fd29864d815e4c2cf5
Deirdre Hegarty <deeadoora@hotmail.com>
2016-10-18 21:51:01 +0100

    styled version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # http://getbootstrap.com/components/#glyphicons
   def glyphicon(glyph, options = {})
-    options[:class] = "glyphicon glyphicon-#{glyph}" + options[:class].to_s
+    options[:class] = "glyphicon glyphicon-#{glyph}"  + ' ' + options[:class].to_s
     options["aria-hidden"] = true
     content_tag :span, '', options
   end
