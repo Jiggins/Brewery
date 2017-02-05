1877a87c05c262cf5ad3a8fcea055141cf2e3fac
Jack Higgins <jackhiggins07@gmail.com>
2017-02-05 22:48:50 +0000

    Seperate user and account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = Account.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+    session[:auth_id] = user.id
     redirect_to root_path
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:auth_id] = nil
     redirect_to root_path
   end
 end
