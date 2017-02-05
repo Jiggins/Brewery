0167153b914f8bc8237c0b637110a580bd5ac51b
Jack Higgins <jackhiggins07@gmail.com>
2017-02-05 17:30:16 +0000

    Moved home to till for i in $(find app | grep home); do mv $i ${i//home/till}; done

---
+++ app/controllers/till_controller.rb
@@ -0,0 +1,4 @@
+class TillController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'till#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
