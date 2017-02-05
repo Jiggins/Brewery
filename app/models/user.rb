993c297af0c84ae84be3aad535ae24e861c6b5d5
Jack Higgins <jackhiggins07@gmail.com>
2017-02-05 22:49:21 +0000

    Add image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :sales
+  has_attached_file :image
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
