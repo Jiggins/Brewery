7e1a28166b32976fb486afa4bc742b146390a180
jackhigg <jackhigg@amazon.com>
2017-02-20 20:53:14 +0000

    Logging the login process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,10 @@ class ApplicationController < ActionController::Base
 
   def force_login
     if ! auth_account
+      Rails.logger.info "Account not logged in, redirecting to #{login_url}"
       redirect_to login_url
     elsif ! current_user
+      Rails.logger.info "User not logged in, redirecting to #{users_path} for account #{auth_account.name}, #{auth_account.uid}"
       redirect_to users_path
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
 
   def create
     account = Account.from_omniauth(env["omniauth.auth"])
+    Rails.logger.info "Successful login for #{account.name}, id: #{account.id}, uid: #{account.uid}"
     session[:auth_id] = account.id
-    redirect_to till_path
+    redirect_to users_path
   end
 
   def destroy
+    Rails.logger.info "Logging out #{account.name}, id: #{account.id}, uid: #{account.uid}"
     session[:auth_id] = nil
     session[:user_id] = nil
     redirect_to root_path
